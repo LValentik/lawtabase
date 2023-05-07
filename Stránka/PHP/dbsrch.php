0f835b42ded1d32de15b00fbe3e928d79828d706
Lukáš Valentík <113976719+LValentik@users.noreply.github.com>
2023-05-07 18:46:13 +0200

    Add files via upload
    Projekt funguje, zbývají grafické úpravy

--- Stránka/PHP/dbsrch.php
+++ Stránka/PHP/dbsrch.php
@@ -1,8 +1,8 @@
 <?php
-
 $law = $_POST['lawtype'];
 $legislative = $_POST['legislative'];
-$search = $_POST['search'];
+$search_parag = $_POST['search_parag'];
+$search_law = $_POST['search_law'];
 $dbname = 'Lawtabase';
 $dbuser = 'postgres';
 $dbpass = 'VaL123eNt456Ik789.';
@@ -11,10 +11,23 @@ $dbport = '5432';
 
 $conn = pg_connect("host=$dbhost port=$dbport dbname=$dbname user=$dbuser password=$dbpass");
 
-$sql = "SELECT * FROM $law.$legislative WHERE law LIKE '%$search%'";
+$sql = "SELECT * FROM $legislative.$law WHERE paragraph_num = '$search_parag' OR law LIKE '$search_law'";
 $result = pg_query($conn, $sql);
 
-echo $result;
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+echo "<table>\n";
+while ($row = pg_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['paragraph_num'] . "</td>";
+    echo "<td>" . $row['law'] . "</td>";
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+pg_free_result($result);
 pg_close($conn);
 ?>
-
