300069cdf4b79c3086c77af75967885e617abcc6
Lukáš Valentík <113976719+LValentik@users.noreply.github.com>
2023-05-07 21:46:38 +0200

    Delete JS directory

--- JS/lawtabase-js.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("search-button").addEventListener("click", function() {
-    var query = document.getElementById("search").value;
-    var quer2 = document.getElementById("legislative").value;
-    console.log("You searched for: " + query + quer2);
-    alert("You searched for: " + query + quer2);
-  });
